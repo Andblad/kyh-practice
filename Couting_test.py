cea9045504b4df513bcf9239fc904f55b9bbf211
Andblad <tobbe_andblad@hotmail.com>
2020-08-31 15:38:33 +0200

    nytt projekt

---
+++ Couting_test.py
@@ -0,0 +1,16 @@
+nummer = 0
+total = 0
+while True :
+    print("För att avsluta, ange avslut")
+    x = input ("Ange ett nummer:")
+    if x == "avslut":
+        break
+    try:
+        floatvar = float(x)
+    except:
+        print("ogiltlig input")
+        continue
+    nummer = nummer + 1
+    total = total + floatvar
+print("Här är totalen,antal input, och genomsnittet:")
+print(total,nummer, total/nummer)
