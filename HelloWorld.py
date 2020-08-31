ed81bafd4e17e031f06235f90dffe5776d2ace57
Andblad <tobbe_andblad@hotmail.com>
2020-08-31 14:10:17 +0200

    change + nytt projekt

--- HelloWorld.py
+++
@@ -1,3 +0,0 @@
-print("Mitt namn är Tobias, och jag är uppväxt i Göteborg")
-print("Jag har tidigare programerat i C++ till och från.")
-print("Jag sökte till KYH PvT för att jag tycket att utbildningen lät spänande.")
--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-print("Hello World")
-print("1 2 3.")
+print("Mitt namn är Tobias, och jag är uppväxt i Göteborg")
+print("Jag har tidigare programerat i C++ till och från.")
+print("Jag sökte till KYH PvT för att jag tycket att utbildningen lät spänande.")
