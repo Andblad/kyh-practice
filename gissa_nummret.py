d92acd3224bcfd89180a4d130d5774a7d358e6e6
Andblad <tobbe_andblad@hotmail.com>
2020-09-01 13:29:14 +0200

    mainloop now gives tal value.

--- gissa_nummret.py
+++ gissa_nummret.py
@@ -1,27 +1,35 @@
 import random
 
+tal = 0
 n = random.randint(1, 100)
 print("Jag tänker på ett tal mellan 1 och 100, vilket?")
+
 
 def ask_number():
     text = input("Din gissning: ")
     as_number = int(text)
     return as_number
 
-def mainloop(n):
 
-
+def mainloop(n,tal):
     while True:
+
         as_number = ask_number()
+        tal = tal + 1
         if as_number == n:
-         print("Korekt!")
-         break
+            print("Korekt!")
+
+            break
 
         if as_number < n:
             print("Fel, mitt tal är högre... Testa igen!")
 
         if as_number > n:
             print("Fel, mitt tal är lägre... testa igen!")
-    return
 
-mainloop(n)
+    return tal
+
+tal = mainloop(n,tal)
+
+
+print("Du gissade", tal, "antal gånger")
