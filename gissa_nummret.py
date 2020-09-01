6c84219931cb20901d08f87e9a31743db61719b4
Andblad <tobbe_andblad@hotmail.com>
2020-09-01 11:03:38 +0200

    nytt program

--- gissa_nummret.py
+++ gissa_nummret.py
@@ -2,20 +2,22 @@ import random
 antal_gissngar = 0
 n = random.randint(1, 100)
 print("Jag tänker på ett tal mellan 1 och 100, vilket?")
+def ask_number():
 
-while True:
-    text = input("Din gissning: ")
-    as_number = int(text)
-    antal_gissngar = antal_gissngar + 1
-    if as_number == n:
-        print("Korekt!")
+def mainloop():
+    while True:
+        text = input("Din gissning: ")
+        as_number = int(text)
 
+        if as_number == n:
+         print("Korekt!")
         break
 
-    if as_number < n:
-        print("Fel, mitt tal är högre... Testa igen!")
+        if as_number < n:
+            print("Fel, mitt tal är högre... Testa igen!")
 
-    if as_number > n:
-        print("Fel, mitt tal är lägre... testa igen!")
+        if as_number > n:
+            print("Fel, mitt tal är lägre... testa igen!")
+        return
 
-print("Du gissade" , antal_gissngar ,"antal gånger.")
+mainloop()
