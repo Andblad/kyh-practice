d06de62dd6647f82ed2e90d38efa23f4680c4a88
Andblad <tobbe_andblad@hotmail.com>
2020-09-01 13:55:32 +0200

    ny repo, nya inställingar, nytt program.

---
+++ extra.py
@@ -0,0 +1,5 @@
+#kolla om en  bokstav är vokal eller ej
+ch = input("Ange en bokstav för att se om de tär en vokal eller ej:")
+if ch = 0
+    break
+elif ch = 
--- test.py
+++
@@ -1,2 +0,0 @@
-def additon(a,b)
-    c=
