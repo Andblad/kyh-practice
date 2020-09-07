3221a4cabcd2c46491184205eb30cf9cef0ebc38
Andblad <tobbe_andblad@hotmail.com>
2020-09-07 11:10:35 +0200

    update to up12(.4)

--- uppgift12.py
+++ uppgift12.py
@@ -1,17 +1,23 @@
-def is_it_too_long(name):
+def is_it_too_long(name, antal):
 
-    return len(name) > 5
+    return len(name) > antal
 
-def main():
-
-
+def main(antal):
 
 
-    students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
+    students=input("Ange studenters namn med , imellan:").split(",")
+
+    #students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
     for name in students:
 
-        if is_it_too_long(name):
-            print(f"{name} är för långt!")
+        if is_it_too_long(name,antal):
+            print(f"{name.capitalize()} är för långt!")
 
 if __name__ == '__main__':
-    main()
+
+    try:
+        antal = int(input("Ange antal bokstäver som maxlängd för namn: "))
+    except ValueError:
+        print("Heltal ej anget. Sätter antal bokstäver till 5.")
+        antal=5
+    main(antal)
