f4442a851e8f5db32f48fade19373e60f7430d5d
Andblad <tobbe_andblad@hotmail.com>
2020-09-07 13:57:35 +0200

    new uppgift 13

---
+++ uppgift13.py
@@ -0,0 +1,18 @@
+import random
+
+väg = 'beställas på posten','laddas ner','köpas på ica','lånas på bilioteket','hämtas på din vårdcentral'
+nat = ['svensk','tysk','dansk','norman','finnländare']
+sjukdom = ['corronavirus','vattkoppor','röda hund','förkylning','HIV']
+koms = ['komma nära','chatta med','åka bil med','äta middiga med','bråka med']
+
+x = random.randint(1,2,3,4,5)
+
+
+def main(väg,nat,sjukdom,koms):
+
+    print(f"En app som kan {random.choices(väg)} ska varna {random.choices(nat)} som någon {random.choices(koms)} som smittats av {random.choices(sjukdom)} .")
+    print(f"- Du tycker att vi i Helsingland borde strunta i att göra något liknande, säger Jonathan, chef för Svenska institutetför inre säkerhet, SIIS.")
+
+
+if __name__ == '__main__':
+    main(väg,nat,sjukdom,koms)
