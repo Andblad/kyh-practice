6a8fa6bd580948b064dec4a53bc492e37063d68b
Andblad <tobbe_andblad@hotmail.com>
2020-09-10 11:28:43 +0200

    uppdate up18.

--- uppgift18.py
+++ uppgift18.py
@@ -11,7 +11,7 @@ people = {
 def main():
     hända = input("1:Slå upp nummer.\n2:Redigera lista.\nVad vill du göra:")
     if hända == '1':
-        #print(F"Det finns {len(people)} personer i telefonlistan")
+        #print(f"Det finns {len(people)} personer i telefonlistan")
         vem = input('Vem vill du ringa?')
         if vem not in people:
             print('Sorry hörru, vet ej vem detta är. Har endast VIP i min katalog')
@@ -19,10 +19,16 @@ def main():
             number = people[vem]
             print(f"Numret till {vem} är {number}")
     if hända == '2':
-        redi = input("Vill du:\n 1:Lägga till.\n2: Ta bort")
+        redi = input("Vill du:\n 1:Lägga till/ Skriva över.\n2: Ta bort")
         if redi == '1':
-             
+            name = input("Skriv namn:")
+            numb = input ("Ange nummer:")
+            people[name] = numb
+            print(people)
         if redi == '2':
+            tabort = input("Vem vill du ta bort:")
+            del people[tabort]
+            print(people)
 
 if __name__ == '__main__':
     main()
