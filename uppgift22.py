5943e9d67330097b2cb874ee2e356957daabd730
Andblad <tobbe_andblad@hotmail.com>
2020-09-14 15:00:41 +0200

    updates to stuff, new files

---
+++ uppgift22.py
@@ -0,0 +1,36 @@
+import json
+from pathlib import Path
+p = Path('phonelists.json')
+content = p.read_text(encoding='utf8')
+data = json.loads(content)
+people = data
+
+
+def main():
+    print(f'Det finns {len(people)} nummer i kattalogen')
+    hända = input("1:Slå upp nummer.\n2:Redigera lista.\nVad vill du göra:")
+    if hända == '1':
+        vem = input('Vem vill du ringa?')
+        if vem not in people:
+            print('Sorry hörru, vet ej vem detta är. Har endast VIP i min katalog')
+        else:
+            number = people[vem]
+            print(f"Numret till {vem} är {number}")
+    if hända == '2':
+        redi = input("Vill du:\n 1:Lägga till/ Skriva över.\n2: Ta bort")
+        if redi == '1':
+            name = input("Skriv namn:")
+            numb = input ("Ange nummer:")
+            people[name] = numb
+            c = json.dumps(people, indent=2  )
+
+            p.write_text(f"{c}", encoding='utf8')
+            print(people)
+
+        if redi == '2':
+            tabort = input("Vem vill du ta bort:")
+            del people[tabort]
+            print(people)
+
+if __name__ == '__main__':
+    main()
